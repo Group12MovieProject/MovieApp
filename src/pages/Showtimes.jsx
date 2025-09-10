3016782e230fa222ba1880d5d848c5097719da93
Emma <t3taem00@students.oamk.fi>
2025-09-10 15:37:51 +0300

    Showtimes first drawft

--- src/pages/Showtimes.jsx
+++ src/pages/Showtimes.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Showtimes() {
+  const [movies, setMovies] = useState([])
+
+  const getFinnkinoShows = (xml) => {
+    const parser = new DOMParser()
+    const xmlDoc = parser.parseFromString(xml, 'application/xml')
+    const root = xmlDoc.children
+    //console.log(root)
+    const shows = root[0].children
+    //console.log(shows)
+
+    const tempMovies = []
+
+    for (let i = 0; i < shows.length; i++) {
+      console.log(shows[i].children[0].innerHTML) //PubDate
+      console.log(shows[i].children[1].innerHTML) //Shows
+
+      tempMovies.push(
+        {
+          "PubDate": shows[i].children[0].innerHTML,
+          "Shows": shows[i].children[1].innerHTML
+        }
+      )
+    }
+    setMovies(tempMovies)
+  }
+
+  useEffect(() => {
+    fetch('https://www.finnkino.fi/xml/Schedule/')
+      .then(response => response.text())
+      .then(xml => {
+        // console.log(xml)
+        getFinnkinoShows(xml)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }, [])
+
   return (
-    <div>Showtimes</div>
+    <div>
+      <select>
+        {
+          movies.map(movie => (
+            <option>{movie.shows}</option>
+          ))
+        }
+      </select>
+    </div>
   )
 }
+//Showtimes
+//export default Showtimes;
