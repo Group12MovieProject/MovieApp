61f4a58ac1ca3dce2abb8e9b2106be5d6ae760b2
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-09-10 12:38:31 +0300

    Haku toimii

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -7,6 +7,7 @@ export default function Search(){
     const [movies, setMovies] = useState([])
     const [page, setPage] = useState(1)
     const [pageCount, setPageCount] = useState(0)
+    const [query, setQuery] = useState('Alien')
 
 
     const Movies = () => {
@@ -18,13 +19,14 @@ export default function Search(){
             </table>
         )
     }
-    useEffect(() => {
-        fetch('https://api.themoviedb.org/3/search/movie?query=star%20wars&include_adult=false&language=en-US&page=' + page, {
-            headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MGIwMjhhYTk3ODRjMTE3NzJhMjQwOTllNzI5MDVmOCIsIm5iZiI6MTc1NzQ4MzkzNC42NzcsInN1YiI6IjY4YzExMzllYjRiM2JiYjczYjliZDA1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.44SoXEYMvQslmBfz2siV0z0JD5_FRB7cJuDhXwJFsVc',
-                'Content-Type':'application/json'
-            }
-        })
+
+    const search = () => {
+      fetch('https://api.themoviedb.org/3/search/movie?query=' + query + '&include_adult=false&language=en-US&page=' + page, {
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MGIwMjhhYTk3ODRjMTE3NzJhMjQwOTllNzI5MDVmOCIsIm5iZiI6MTc1NzQ4MzkzNC42NzcsInN1YiI6IjY4YzExMzllYjRiM2JiYjczYjliZDA1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.44SoXEYMvQslmBfz2siV0z0JD5_FRB7cJuDhXwJFsVc',
+          'Content-Type':'application/json'
+        }
+    })
         .then(response => response.json())
         .then (json => {
             setMovies(json.results)
@@ -33,11 +35,15 @@ export default function Search(){
         .catch(error => {
             console.log(error)
         })
+    }
+    useEffect(() => {
+      search()
     }, [page])
 
   return (
     <div id="container">
       <h3>Search movies</h3>
+      <input value={query} onChange={e => setQuery(e.target.value)} ></input><button onClick={search} type="button">Search</button>
       <ReactPaginate
         breakLabel="..."
         nextLabel=">"
