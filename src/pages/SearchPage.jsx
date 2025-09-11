bd36fc323df48e76a5e1b887c176cc2c2bb911ae
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-09-11 15:17:14 +0300

    Hakua monipuolistettu

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,5 +1,6 @@
 
 import React, {useState, useEffect} from 'react'
+import placeholder from '../assets/placeholder.png';
 import { useLocation } from 'react-router-dom';
 import ReactPaginate from 'react-paginate'
 import './SearchPage.css'
@@ -16,17 +17,186 @@ export default function Search() {
 
   const ResultsTable = () => {
     if (!results || results.length === 0) return <div>No results</div>;
-    return (
-      <table>
-        <tbody>
-          {results.map(item => (
-            <tr key={item.id}>
-              <td>{searchType === 'movie' ? item.title : item.name}</td>
+    // for multi search results are separated in 3 tables
+    if (searchType === 'multi') {
+      const persons = results.filter(item => item.media_type === 'person');
+      const movies = results.filter(item => item.media_type === 'movie');
+      const tvs = results.filter(item => item.media_type === 'tv');
+
+      return (
+        <div>
+          {persons.length > 0 && (
+            <div style={{marginBottom: '2em'}}>
+              <h4>Persons</h4>
+              <table>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Known for department</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {persons.map(item => (
+                    <tr key={item.id}>
+                      <td>
+                          <img
+                            src={item.profile_path ? `https://image.tmdb.org/t/p/w185${item.profile_path}` : placeholder}
+                          alt={item.name}
+                          style={{ width: '60px', borderRadius: '6px', marginRight: '1em' }}
+                        />
+                      </td>
+                      <td>{item.name}</td>
+                      <td>{item.known_for_department}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+          {movies.length > 0 && (
+            <div style={{marginBottom: '2em'}}>
+              <h4>Movies</h4>
+              <table>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Title</th>
+                    <th>Release date</th>
+                    <th>TMDB Score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {movies.map(item => (
+                    <tr key={item.id}>
+                      <td>
+                        <img
+                          src={item.poster_path ? `https://image.tmdb.org/t/p/w185${item.poster_path}` : placeholder}
+                          alt={item.title}
+                          style={{ width: '60px', borderRadius: '6px', marginRight: '1em' }}
+                        />
+                      </td>
+                      <td>{item.title}</td>
+                      <td>{item.release_date}</td>
+                      <td>{item.vote_average}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+          {tvs.length > 0 && (
+            <div style={{marginBottom: '2em'}}>
+              <h4>TV Shows</h4>
+              <table>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Title</th>
+                    <th>First air date</th>
+                    <th>TMDB score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tvs.map(item => (
+                    <tr key={item.id}>
+                      <td>
+                        <img
+                          src={item.poster_path ? `https://image.tmdb.org/t/p/w185${item.poster_path}` : placeholder}
+                          alt={item.name}
+                          style={{ width: '60px', borderRadius: '6px', marginRight: '1em' }}
+                        />
+                      </td>
+                      <td>{item.name}</td>
+                      <td>{item.first_air_date}</td>
+                      <td>{item.vote_average}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      );
+    }
+    // other searches like before (only shows what user is searching)
+    if (searchType === 'person') {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Known for department</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    )
+          </thead>
+          <tbody>
+            {results.map(item => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.known_for_department}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
+    }
+    if (searchType === 'movie') {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Title</th>
+              <th>Release date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map(item => (
+              <tr key={item.id}>
+                <td>
+                  <img
+                    src={item.poster_path ? `https://image.tmdb.org/t/p/w185${item.poster_path}` : placeholder}
+                    alt={item.title}
+                    style={{ width: '60px', borderRadius: '6px', marginRight: '1em' }}
+                  />
+                </td>
+                <td>{item.title}</td>
+                <td>{item.release_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
+    }
+    if (searchType === 'tv') {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Title</th>
+              <th>First air date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map(item => (
+              <tr key={item.id}>
+                <td>
+                  <img
+                    src={item.poster_path ? `https://image.tmdb.org/t/p/w185${item.poster_path}` : placeholder}
+                    alt={item.name}
+                    style={{ width: '60px', borderRadius: '6px', marginRight: '1em' }}
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.first_air_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
+    }
+    return null;
   }
 
   const search = (customQuery = query, customPage = page) => {
