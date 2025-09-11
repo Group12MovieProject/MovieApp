579e5615ccc89674cd5e580ace5c296db63e5db4
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-09-11 10:12:29 +0300

    tmdb api key piilotettu .enviin

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,9 @@
+
 import React, {useState, useEffect} from 'react'
 import ReactPaginate from 'react-paginate';
 import './SearchPage.css'
+
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export default function Search(){
 
@@ -25,7 +28,7 @@ export default function Search(){
     const search = () => {
       fetch('https://api.themoviedb.org/3/search/movie?query=' + query + '&include_adult=false&language=en-US&page=' + page, {
         headers: {
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MGIwMjhhYTk3ODRjMTE3NzJhMjQwOTllNzI5MDVmOCIsIm5iZiI6MTc1NzQ4MzkzNC42NzcsInN1YiI6IjY4YzExMzllYjRiM2JiYjczYjliZDA1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.44SoXEYMvQslmBfz2siV0z0JD5_FRB7cJuDhXwJFsVc',
+          'Authorization': 'Bearer ' + TMDB_API_KEY,
           'Content-Type':'application/json'
         }
     })
