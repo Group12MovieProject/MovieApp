bc8940a0cce45eec06d93610f2d38e95d1f5b5b7
Kata <kata.niva03@gmail.com>
2025-09-16 11:13:28 +0300

    serverin alustus jatkettu

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 import express from 'express'
 import cors from 'cors'
+import userRouter from './routes/userRouter.js'
 
-const port = 3001
+const port = process.env.PORT
 
 const app = express()
 app.use(cors())
-
-app.get('/', (req, res) => {
-    res.status(200).json({result: "Success"})
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/user',userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
+})
+
+app.use((err,req,res,next) => {
+    const statusCode = err.status || 500
+    res.status(statusCode).json({
+        error:{
+            message: err.message,
+            status: statusCode
+        }
+    })
 })
