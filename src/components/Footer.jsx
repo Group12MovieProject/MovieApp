1baec8e1554d7c23543eeb88cea34036063cb0c1
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-09-17 15:27:02 +0300

    responsiivisuutta footer, header ja nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='footer'>
-        <div className='container'>
+        <div className='footer-content'>
             <span className='text-muted'>Footer here...</span>
         </div>   
     </footer>
