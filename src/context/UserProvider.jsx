0c63e258b5d6ae01fbe7a610af39df4929e7a319
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-09-22 18:00:50 +0300

    päivitetty auth middleware ja userprovider

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { UserContext } from './UserContext.jsx';
 import axios from 'axios'
 
