5e17838506013016c2def54d1108c48693755507
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-10-13 18:29:21 +0300

    error 500 yritetty korjata

--- server/helper/db.js
+++ server/helper/db.js
@@ -1,9 +1,9 @@
 import pkg from 'pg'
 import dotenv from 'dotenv'
 
-const environment = process.env.NODE_ENV || 'development'
-
 dotenv.config()
+
+const environment = process.env.NODE_ENV || 'development'
 
 const { Pool } = pkg
 
@@ -11,8 +11,7 @@ const openDb = () => {
     const pool = new Pool({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
-        database: environment === "development" ? process.env.DB_NAME :
-        process.env.TEST_DB_NAME,
+        database: environment === "test" ? process.env.TEST_DB_NAME : process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
         ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
